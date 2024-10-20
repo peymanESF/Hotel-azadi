447ff199b51f233e0204d7f86b7edf6dfc149824
p.esfandmazd <p.esfandmazd@douran.ir>
2024-10-20 16:32:07 +0330

    2024-10-20-add about hotel section

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
-  $(".owl-carousel").owlCarousel({
-	  items: 4,
-      rtl: true,
-      loop: true,
-      dots: true,
-      nav: true,
-      navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
-      autoplay: true,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true,
-	  responsive:{
-		0:{
-			items:1
-		},
-		600:{
-			items:2
-		},
-		1000:{
-			items:4
+$(document).ready(function () {
+	/**$(".owl-carousel").owlCarousel({
+		items: 4,
+		rtl: true,
+		loop: true,
+		dots: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 4
+			}
 		}
-	  }
-  });
-  
+	});**/
+	// تاریخ‌نگار فارسی
+
 });
