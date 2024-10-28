aa6188253dd2bd3ceed8c272ab90db47253f95f7
p.esfandmazd <p.esfandmazd@douran.ir>
2024-10-28 17:22:56 +0330

    bug fixes - thme color and change language btn

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,29 +1,5 @@
 $(document).ready(function () {
-	/**$(".owl-carousel").owlCarousel({
-		items: 4,
-		rtl: true,
-		loop: true,
-		dots: true,
-		nav: true,
-		navText: ['<i class="fa fa-angle-right"></i>', '<i class="fa fa-angle-left"></i>'],
-		autoplay: true,
-		autoplayTimeout: 5000,
-		autoplayHoverPause: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 4
-			}
-		}
-	});**/
-	// تاریخ‌نگار فارسی
-
-    
-
-
+		$('.changeLanguage li a').click(function(){
+		$(this).siblings('ul').toggleClass('show');
+	});
 });
